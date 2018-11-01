fdf03a5eebf3e5dce316b8047f762eda787b24fd
AlbertoCota93 <is694734@iteso.mx>
2018-11-01 15:41:32 -0600

    add product finished

--- app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityItem.java
+++ app/src/main/java/com/iteso/pdm18_scrollabletabs/ActivityItem.java
@@ -1,17 +1,22 @@
 package com.iteso.pdm18_scrollabletabs;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iteso.pdm18_scrollabletabs.beans.Category;
+import com.iteso.pdm18_scrollabletabs.beans.ItemProduct;
 import com.iteso.pdm18_scrollabletabs.beans.Store;
 import com.iteso.pdm18_scrollabletabs.database.CategoryControl;
 import com.iteso.pdm18_scrollabletabs.database.DataBaseHandler;
+import com.iteso.pdm18_scrollabletabs.database.ItemProductControl;
 import com.iteso.pdm18_scrollabletabs.database.StoreControl;
 
 import java.util.ArrayList;
@@ -20,15 +25,17 @@ public class ActivityItem extends AppCompatActivity {
     StoreControl storeControl;
     CategoryControl categoryControl;
     DataBaseHandler dh;
+    ItemProduct itemProduct;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
-        Spinner spin_photo;
-        Spinner spin_store;
-        Spinner spin_category;
-        EditText item_name;
+        final Spinner spin_photo;
+        final Spinner spin_store;
+        final Spinner spin_category;
+        final EditText item_name;
         Button save;
 
         spin_photo = findViewById(R.id.item_photo_spinner);
@@ -45,7 +52,7 @@ public class ActivityItem extends AppCompatActivity {
 
         ArrayList<Category> categories;
         categories = categoryControl.getCategory(dh);
-        ArrayAdapter<Category> cats = new ArrayAdapter<>(this,
+        final ArrayAdapter<Category> cats = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item,categories);
         spin_category.setAdapter(cats);
 
@@ -55,6 +62,23 @@ public class ActivityItem extends AppCompatActivity {
                 android.R.layout.simple_spinner_dropdown_item,Stores);
         spin_store.setAdapter(stores);
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if(item_name.getText().equals("")){
+                    Toast.makeText(ActivityItem.this,"Insert Item Text", Toast.LENGTH_LONG).show();
+                } else {
+
+                    itemProduct.setImage((int) spin_photo.getSelectedItem());
+                    itemProduct.setTitle(item_name.getText().toString());
+                    itemProduct.setCategory((Category) spin_category.getSelectedItem());
+                    itemProduct.setStore((Store) spin_store.getSelectedItem());
+
+                    ItemProductControl itemProductControl = new ItemProductControl();
+                    itemProductControl.addProduct(itemProduct, dh);
+                }
+            }
+        });
     }
 }
